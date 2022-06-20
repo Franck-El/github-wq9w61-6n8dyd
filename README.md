8f75b9ee8bb1f5997fb40a38e598273a5871b6b0
Franck-El <spyki095@gmail.com>
2022-06-20 14:58:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-wq9w61-6n8dyd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-wq9w61-6n8dyd)
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path'); // require car webpack fonctionne sous node
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js', // commencer par le fichier index.js (chemin relatif)
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  module: {
+    rules: [
+      // regles : si c'est un css, faire ca, si c'est du ts faire ca, etc.
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'], // (respecter cet ordre)
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  output: {
+    // sortir un fichier bundle.js dans __dirname/dist (__dirname etant le dossier courant), path.resolve le transforme en chemin absolu, necessaire ici
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`,
+      new CssMinimizerPlugin(),
+    ],
+  },
+};
